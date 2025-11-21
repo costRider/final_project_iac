f39972976eea4a8cfb10be01f68196e55d12342a
Ubuntu <insulin90@gmail.com>
2025-11-21 16:44:28 +0000

    fixed network -local

--- aws/modules/network/locals.tf
+++ aws/modules/network/locals.tf
@@ -3,7 +3,7 @@
 locals {
   region_code = coalesce(
     var.region_code,
-    replace(regexreplace(var.aws_region, "(ap)-(northeast)-(\d)", "$1-$2-$3"), "ap-northeast-", "apne")
+     replace(var.aws_region, "-", "")
   )
 
   name_prefix = "${var.project_name}-${var.environment}-aws-${local.region_code}"
