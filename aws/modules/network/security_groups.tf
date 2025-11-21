bd80e9c2d5fdd38d1d4b653e560ea3c8bccf35ad
MIN KYU LEE <107491279+costRider@users.noreply.github.com>
2025-11-22 01:51:03 +0900

    Refactor security group rules into attachments

--- aws/modules/network/security_groups.tf
+++ aws/modules/network/security_groups.tf
@@ -5,21 +5,6 @@ resource "aws_security_group" "bastion" {
   description = "Bastion SSH"
   vpc_id      = aws_vpc.this.id
 
-  ingress {
-    description = "Allow SSH From MyIP"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.my_ip_cidr]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = merge(var.common_tags, {
     Name = "${local.name_prefix}-net-sg-bastion-01"
   })
@@ -29,21 +14,6 @@ resource "aws_security_group" "mgmt" {
   name        = "${local.name_prefix}-mgmt-sg-control-01"
   description = "SSH From bastion to MGMT"
   vpc_id      = aws_vpc.this.id
-
-  ingress {
-    description     = "Allow SSH from bastion SG"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.bastion.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = merge(var.common_tags, {
     Name = "${local.name_prefix}-mgmt-sg-control-01"
@@ -55,22 +25,64 @@ resource "aws_security_group" "node" {
   description = "Private node security group"
   vpc_id      = aws_vpc.this.id
 
-  ingress {
-    description = "Allow all inbound traffic from VPC"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [var.vpc_cidr]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = merge(var.common_tags, {
     Name = "${local.name_prefix}-app-sg-node-01"
   })
+}
+
+resource "aws_security_group_rule" "bastion_ingress_ssh" {
+  type              = "ingress"
+  description       = "Allow SSH From MyIP"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = [var.my_ip_cidr]
+  security_group_id = aws_security_group.bastion.id
+}
+
+resource "aws_security_group_rule" "bastion_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion.id
+}
+
+resource "aws_security_group_rule" "mgmt_ingress_ssh_from_bastion" {
+  type                     = "ingress"
+  description              = "Allow SSH from bastion SG"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.bastion.id
+  security_group_id        = aws_security_group.mgmt.id
+}
+
+resource "aws_security_group_rule" "mgmt_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.mgmt.id
+}
+
+resource "aws_security_group_rule" "node_ingress_all_from_vpc" {
+  type              = "ingress"
+  description       = "Allow all inbound traffic from VPC"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = [var.vpc_cidr]
+  security_group_id = aws_security_group.node.id
+}
+
+resource "aws_security_group_rule" "node_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.node.id
 }
